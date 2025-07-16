4a6650f3bfad5974f6ea476d3b04e4ca4933871b
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-07-16 11:39:58 +0700

    DIKIT OI

--- resources/views/layouts/divisi.blade.php
+++ resources/views/layouts/divisi.blade.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard {{ $divisiNama }}</title>
+    <title> Mestakara {{ $divisiNama }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
