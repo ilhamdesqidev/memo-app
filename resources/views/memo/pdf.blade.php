e06e0a8514d54965cbc7221929e10969fab97aa0
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-07-29 15:51:56 +0700

    Doraemon kotak

--- resources/views/memo/pdf.blade.php
+++ resources/views/memo/pdf.blade.php
@@ -4,17 +4,100 @@
     <meta charset="utf-8">
     <title>Memo - {{ $memo->nomor }}</title>
     <style>
-        body { font-family: Arial, sans-serif; line-height: 1.6; }
-        .header { text-align: center; margin-bottom: 20px; }
-        .content { margin: 0 auto; width: 90%; }
-        .footer { margin-top: 50px; }
-        .signature-container { margin-top: 50px; display: flex; justify-content: flex-end; }
-        .signature-box { width: 300px; text-align: center; }
-        .signature-img { height: 80px; margin-bottom: 10px; }
-        .signature-line { border-top: 1px solid #000; width: 200px; margin: 0 auto; }
-        .signature-name { margin-top: 5px; font-weight: bold; }
-        .signature-position { font-size: 0.9em; }
-        .signature-date { font-size: 0.9em; margin-top: 5px; }
+        body { 
+            font-family: Arial, sans-serif; 
+            line-height: 1.6; 
+            color: #000;
+            margin: 0;
+            padding: 20px;
+        }
+        
+        .header { 
+            text-align: center; 
+            margin-bottom: 30px; 
+            border-bottom: 1px solid #000;
+            padding-bottom: 15px;
+        }
+        
+        .header h2 {
+            margin: 0 0 5px 0;
+            font-size: 18px;
+            font-weight: bold;
+        }
+        
+        .header h3 {
+            margin: 0;
+            font-size: 14px;
+            font-weight: normal;
+        }
+        
+        .content { 
+            margin: 0 auto; 
+            width: 90%; 
+        }
+        
+        .memo-details table {
+            width: 100%;
+            margin-bottom: 25px;
+        }
+        
+        .memo-details td {
+            padding: 8px 0;
+            vertical-align: top;
+        }
+        
+        .memo-details .label {
+            width: 120px;
+            font-weight: bold;
+        }
+        
+        .memo-body {
+            margin: 25px 0;
+            text-align: justify;
+            line-height: 1.7;
+        }
+        
+        .attachment-info {
+            margin: 20px 0;
+            font-weight: bold;
+        }
+        
+        .signature-container { 
+            margin-top: 50px; 
+            display: flex; 
+            justify-content: flex-end; 
+        }
+        
+        .signature-box { 
+            width: 250px; 
+            text-align: center; 
+        }
+        
+        .signature-img { 
+            max-height: 60px; 
+            margin: 10px 0; 
+        }
+        
+        .signature-line { 
+            border-top: 1px solid #000; 
+            width: 200px; 
+            margin: 15px auto; 
+        }
+        
+        .signature-name { 
+            font-weight: bold; 
+            margin-top: 5px;
+        }
+        
+        .signature-position { 
+            font-size: 14px; 
+            margin-top: 3px;
+        }
+        
+        .signature-date { 
+            font-size: 12px; 
+            margin-top: 8px; 
+        }
     </style>
 </head>
 <body>
@@ -24,63 +107,59 @@
     </div>
     
     <div class="content">
-        <table width="100%" cellspacing="0" cellpadding="5">
-            <tr>
-                <td width="20%">Dari</td>
-                <td width="80%">: {{ $memo->dari }}</td>
-            </tr>
-            <tr>
-                <td>Kepada</td>
-                <td>: {{ $memo->kepada }}</td>
-            </tr>
-            <tr>
-                <td>Tanggal</td>
-                <td>: {{ $memo->tanggal->format('d/m/Y') }}</td>
-            </tr>
-            <tr>
-                <td>Perihal</td>
-                <td>: {{ $memo->perihal }}</td>
-            </tr>
-        </table>
+        <div class="memo-details">
+            <table>
+                <tr>
+                    <td class="label">Dari</td>
+                    <td>: {{ $memo->dari }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Kepada</td>
+                    <td>: {{ $memo->kepada }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Tanggal</td>
+                    <td>: {{ $memo->tanggal->format('d/m/Y') }}</td>
+                </tr>
+                <tr>
+                    <td class="label">Perihal</td>
+                    <td>: {{ $memo->perihal }}</td>
+                </tr>
+            </table>
+        </div>
         
-        <div style="margin-top: 30px;">
+        <div class="memo-body">
             {!! nl2br(e($memo->isi)) !!}
         </div>
         
         @if($memo->lampiran > 0)
-        <div style="margin-top: 20px;">
-            <strong>Lampiran:</strong> {{ $memo->lampiran }} lembar
+        <div class="attachment-info">
+            Lampiran: {{ $memo->lampiran }} lembar
         </div>
         @endif
         
-        <!-- Bagian Tanda Tangan -->
         @if($memo->include_signature && $memo->signature_path && $memo->disetujuiOleh)
         <div class="signature-container">
             <div class="signature-box">
-                <div>
-                    @php
-                        $signaturePath = storage_path('app/public/' . $memo->signature_path);
-                        if (file_exists($signaturePath)) {
-                            $imageData = base64_encode(file_get_contents($signaturePath));
-                            $imageInfo = getimagesize($signaturePath);
-                            $mimeType = $imageInfo['mime'];
-                        }
-                    @endphp
-                    
-                    @if(isset($imageData))
-                        <img src="data:{{ $mimeType }};base64,{{ $imageData }}" 
-                            class="signature-img" alt="Tanda Tangan" style="width: 150px; height: auto;">
-                    @else
-                        <div style="color: red; border: 1px dashed #ccc; padding: 5px;">
-                            Tanda tangan tidak ditemukan atau tidak dapat dibaca
-                        </div>
-                    @endif
-                </div>
+                @php
+                    $signaturePath = storage_path('app/public/' . $memo->signature_path);
+                    if (file_exists($signaturePath)) {
+                        $imageData = base64_encode(file_get_contents($signaturePath));
+                        $imageInfo = getimagesize($signaturePath);
+                        $mimeType = $imageInfo['mime'];
+                    }
+                @endphp
+                
+                @if(isset($imageData))
+                    <img src="data:{{ $mimeType }};base64,{{ $imageData }}" 
+                        class="signature-img" alt="Tanda Tangan">
+                @endif
+                
                 <div class="signature-line"></div>
                 <div class="signature-name">{{ $memo->disetujuiOleh->name }}</div>
                 <div class="signature-position">{{ $memo->disetujuiOleh->jabatan }}</div>
                 <div class="signature-date">
-                    Disetujui pada: {{ $memo->approval_date->format('d/m/Y H:i') }}
+                    {{ $memo->approval_date->format('d/m/Y H:i') }}
                 </div>
             </div>
         </div>
