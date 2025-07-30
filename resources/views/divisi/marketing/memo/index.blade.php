d88b7aaa37814dfd8408cfff7616055ede788622
ilham.desqi <ilhamdesqidev@gmail.com>
2025-07-30 13:21:49 +0700

    pesan pdf tidak tersedia

--- resources/views/divisi/marketing/memo/index.blade.php
+++ resources/views/divisi/marketing/memo/index.blade.php
@@ -163,7 +163,7 @@
                                                 </svg>
                                             </a>
                                         @else
-                                            <span class="p-2.5 text-gray-300 cursor-not-allowed rounded-xl" title="PDF tidak tersedia">
+                                            <span class="p-2.5 text-gray-300 cursor-not-allowed rounded-xl" title="PDF tersedia hanya untuk memo yang disetujui">
                                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                                                 </svg>
