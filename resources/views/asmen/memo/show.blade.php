0af40b74f066bf3316b589df7b6a338c6fa77195
ilham desqi <164368396+ilhamdesqidev@users.noreply.github.com>
2025-08-15 08:50:07 +0700

    hapus teruskan ke staff

--- resources/views/asmen/memo/show.blade.php
+++ resources/views/asmen/memo/show.blade.php
@@ -82,20 +82,6 @@
                                 <input type="radio" id="action_forward_manager" name="next_action" value="forward_manager" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300">
                                 <label for="action_forward_manager" class="ml-2 block text-sm text-gray-700">Setujui dan teruskan ke Manager</label>
                             </div>
-                            <div class="flex items-center">
-                                <input type="radio" id="action_forward_staff" name="next_action" value="forward_staff" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300">
-                                <label for="action_forward_staff" class="ml-2 block text-sm text-gray-700">Setujui dan teruskan ke Staff</label>
-                            </div>
-                            
-                            <div id="staffSelection" class="hidden ml-6 mt-2">
-                                <label for="forward_to_staff_id" class="block text-sm font-medium text-gray-700">Pilih Staff</label>
-                                <select name="forward_to_staff_id" id="forward_to_staff_id" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md">
-                                    <option value="">-- Pilih Staff --</option>
-                                    @foreach($staffList as $staff)
-                                        <option value="{{ $staff->id }}">{{ $staff->name }} ({{ $staff->divisi->nama }})</option>
-                                    @endforeach
-                                </select>
-                            </div>
                         </div>
                     </div>
                     
